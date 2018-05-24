d25e3179c092468a600918a4aace2c3ad02dcf06
TobiasGomez <tobi.gomez97@gmail.com>
2018-05-24 10:39:02 -0300

    Inicio de sesion

---
+++ iniciar.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="estilos.css">
+	<title> UnAventon</title>
+	<script  type="text/javascript" src="funcionesvalidacion.js"> </script>
+</head>
+<body class="body">
+<?php 
+	include 'archivoconexion.php';
+	$link = conectar();
+    mysqli_set_charset($link, "utf8");
+	include 'barra.php';
+?>	
+<div class="registrar">
+	<h1 style="color: black;margin-left:80px"> Inicia sesion </h1> 
+			<form  name="regis" method="post" action="Acceso.php" class="input" onsubmit="return sesion()">
+				<label style="color: black"> Mail </label>
+				<input type="text" id="mail" name="mail" class="caballo" placeholder="Mail...">
+				<label style="color: black"> Contraseña </label>
+				<input type="password" id="contrasena" name="contrasena" class="caballo" placeholder="Contraseña...">
+				<div><input type="submit" class="botonRegistrarse" value="Iniciar sesion"></div>
+			</form>
+		</div>
+	</div>
+</div>
+</body>
+</html>
