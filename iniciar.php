69cce6f8b13dfe5fe97d439e60d6440c0d09b99b
TobiasGomez <tobi.gomez97@gmail.com>
2018-05-28 14:45:39 -0300

    Inicio de sesion con HTML5

--- iniciar.php
+++ iniciar.php
@@ -15,9 +15,9 @@
 	<h1 style="color: black;margin-left:80px"> Inicia sesion </h1> 
 			<form  name="regis" method="post" action="Acceso.php" class="input" onsubmit="return sesion()">
 				<label style="color: black"> Mail </label>
-				<input type="text" id="mail" name="mail" class="caballo" placeholder="Mail...">
+				<input type="text" id="mail" name="mail" class="caballo" placeholder="Mail..." required/>
 				<label style="color: black"> Contraseña </label>
-				<input type="password" id="contrasena" name="contrasena" class="caballo" placeholder="Contraseña...">
+				<input type="password" id="contrasena" name="contrasena" class="caballo" placeholder="Contraseña..." required/>
 				<div><input type="submit" class="botonRegistrarse" value="Iniciar sesion"></div>
 			</form>
 		</div>
