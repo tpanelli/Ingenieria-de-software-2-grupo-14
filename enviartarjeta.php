44ea231cb651b977dad124473f1702931384f6a0
TobiasGomez <tobi.gomez97@gmail.com>
2018-05-28 14:48:18 -0300

    Registro de tarjeta con HTML5 y sin CVV

--- enviartarjeta.php
+++ enviartarjeta.php
@@ -6,22 +6,21 @@ $num1=$_POST['numero'];
 $num2=$_POST['numero2'];
 $num3=$_POST['numero3'];
 $num4=$_POST['numero4'];
-$pin=$_POST['pin'];
 $num=$num1 . $num2 . $num3 . $num4;
 $f1=$_POST['fecha1'];
 $f2=$_POST['fecha2'];
 $fecha=$f1."/".$f2;
 session_start();
 $mail=$_SESSION['mail'];
-if(empty ($num1) || empty ($num2) || empty ($num3) || empty ($num4) || empty ($pin) || empty ($f1) || empty ($f2)){
+if(empty ($num1) || empty ($num2) || empty ($num3) || empty ($num4) || empty ($f1) || empty ($f2)){
 	header('Location: pagerrores.php?errores=campostarjeta');
 }
-if(!empty ($num) && !empty ($pin) && !empty ($fecha)){
+if(!empty ($num) && !empty ($fecha)){
 	$var="INSERT INTO `usuario_tarjeta`(`mail`, `numerotarjeta`)
 	VALUES ('$mail', '$num')";
 	mysqli_query($link, $var);
-	$vard="INSERT INTO `tarjeta`(`numero`,`pin` ,`fecha`)
-	VALUES ('$num', '$pin', '$fecha')";
+	$vard="INSERT INTO `tarjeta`(`numero`, `fecha`)
+	VALUES ('$num', '$fecha')";
 	mysqli_query($link, $vard);
 }
 else{
--- registrotarjeta.php
+++ registrotarjeta.php
@@ -1,41 +1,38 @@
 <html>
-<head>
-	<link type="text/css" rel="stylesheet" href="estilos.css">
-	<title> UnAventon</title>
-	<script  type="text/javascript" src="funcionesvalidacion.js"> </script>
-</head>
-<body class="body">
 <?php 
 	include 'archivoconexion.php';
 	$link = conectar();
     mysqli_set_charset($link, "utf8");
 	include 'barra.php';
 	
-?>	
-<div class= "registrar">
-<h1 style="color: black;margin-left:80px"> Registra tu tarjeta </h1> 
-		<div class="camporegis">
+?>
+<?php 
+	if($objeto->Logueado()){ ?>
+		<div class= "registrar">
+			<h1 style="color: black;margin-left:80px"> Registra tu tarjeta </h1>
 			<form method="POST" action="enviartarjeta.php" class="input" onsubmit= "return validarRegistro()">
 				<label style="color: black"> Numero de tarjeta </label>
 				<br>
-				<input type="text" class="chico" maxlength="4" name="numero" placeholder="XXXX">
-				- <input type="text" class="chico" maxlength="4" name="numero2" placeholder="XXXX">
-				- <input type="text" class="chico" maxlength="4" name="numero3" placeholder="XXXX">
-				- <input type="text" class="chico" maxlength="4" name="numero4" placeholder="XXXX">
-				<br>
-				<label style="color: black"> Numero de seguridad </label>
-				<br>
-				<input type="text" class="chico" maxlength="4" name="pin" placeholder="CVV">
+				<input type="text" class="chico" maxlength="4" name="numero" placeholder="XXXX"  pattern="[0-9]{4}" required/>
+				- <input type="text" class="chico" maxlength="4" name="numero2" placeholder="XXXX" pattern="[0-9]{4}" required/>
+				- <input type="text" class="chico" maxlength="4" name="numero3" placeholder="XXXX" pattern="[0-9]{4}" required/>
+				- <input type="text" class="chico" maxlength="4" name="numero4" placeholder="XXXX" pattern="[0-9]{4}" required/>
 				<br>
 				<label style="color: black"> Fecha de vencimiento </label>
 				<br>
-				<input type="text" class="chico" maxlength="2" name="fecha1" placeholder="MM"> 
-				/ <input type="text" class="chico" maxlength="2" name="fecha2" placeholder="AA">
+				<input type="text" class="chico" maxlength="2" name="fecha1" placeholder="MM" pattern="[0-9]{2}" required/> 
+				/ <input type="text" class="chico" maxlength="2" name="fecha2" placeholder="AA" pattern="[0-9]{2}" required/>
 				<br><br>
 				<input type="submit" class="botonRegistrarse" value="Enviar">
 			</form>
 		</div>
-	</div>
-</div>
+	<?php 
+	} 
+	else{ ?>
+		<div class= "registrar">
+			<h1 style="color: black;margin:5%;text-align:centre"> Debes estar logueado para registrar una tarjeta </h1>
+			<div><a href="index.php"><button style="margin:10%" class="botonregistrarse"> Volver al inicio </button></a></div>
+		</div>
+	<?php }?>
 </body>
 </html>
