cc02c3eb106cf8be8822bcf3af4e5400fc18118e
tpanelli <tpanelli@users.noreply.github.com>
2018-05-29 10:24:49 -0300

    Registro de vehiculo (frontend y backend)

---
+++ registrarvehiculo.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'archivoconexion.php';
+session_start();
+$mail = $_SESSION['mail'];
+$link = conectar();
+mysqli_set_charset($link, "utf8");
+$patente = $_POST['patente'];
+$modelo = $_POST['modelo'];
+$marca = $_POST['marca'];
+$capacidad = $_POST['capacidad'];
+$color = $_POST ['color'];
+$tipo = $_POST ['tipo'];
+$consulta = "INSERT INTO vehiculo (patente, modelo, marca, capacidad, color, nombretipo, mail";
+$image = $_FILES['imagen'];
+if ($image['size'] != 0){
+	$var = array(
+		'type' => $image['type'],
+		'tmp_name' => $image['tmp_name']
+	);
+	$imagen = addslashes(file_get_contents($var['tmp_name']));
+	$tipoimagen = $var['type'];
+	$consulta.= ", imagen, tipoimagen) VALUES ('$patente', '$modelo', '$marca', '$capacidad', '$color', '$tipo', '$mail', '$imagen', '$tipoimagen')";
+}
+else{
+	$consulta.= ") VALUES ('$patente', '$modelo', '$marca', '$capacidad', '$color', '$tipo', '$mail')";
+}
+mysqli_query ($link, $consulta);
+header('Location: listarvehiculos.php');
