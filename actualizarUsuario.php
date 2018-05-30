3793e500bb9145c33a35e9dfe96c7cee50bdfb09
pabloesposito <37589094+pabloesposito@users.noreply.github.com>
2018-05-30 13:07:22 -0300

    Actualizar usuario con restriccion de edad

--- actualizarUsuario.php
+++ actualizarUsuario.php
@@ -1,6 +1,14 @@
 <?php
   include"archivoconexion.php";
   $link = conectar();
-  mysqli_query($link," UPDATE usuario set nombre ='$_POST[nombre]', apellido = '$_POST[apellido]', fechanac ='$_POST[fecha]' where mail = '$_POST[mail]'"); 
-  header("location: verPerfil.php");
+  $fecha = $_POST['fecha'];
+  $fecha_actual=date("y-m-d");
+  $diff = abs(strtotime($fecha_actual) - strtotime($fecha));
+  $years = floor($diff / (365*60*60*24));
+  if ($years>=18){
+		mysqli_query($link," UPDATE usuario set nombre ='$_POST[nombre]', apellido = '$_POST[apellido]', fechanac ='$_POST[fecha]' where mail = '$_POST[mail]'"); 
+		header("location: verPerfil.php");
+  }else{
+	  header('location:pagerrores.php?errores=edad');
+  }
    ?>
